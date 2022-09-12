cea86ee94f98f6f74cfa6ccb87c48a10d95ba318
Amitesh Verma <vamitesh2409@gmail.com>
2022-09-12 23:23:01 +0530

    Linked List INSERT AT END

--- LinkedList/linkedlist.java
+++ LinkedList/linkedlist.java
@@ -1,10 +1,10 @@
 package LinkedList;
 
 public class linkedlist {
-
+    // Variables
     public Node headNode;           // To hold head element
     public int size;                // To hold the size
-
+    // Class node
     public class Node{
         int data;                   // Hold data of node
         Node nextNode;              // Address of next node
@@ -13,11 +13,13 @@ public class linkedlist {
         }
     }
 
+    // Constructor
     public linkedlist(){
         headNode = null;
         size = 0;
     }
 
+    // Insert at head
     public void insertAtHead(int value){
         Node newNode = new Node(value);     // Creating new node
         newNode.nextNode = headNode;        // Joining the new node to head node
@@ -25,6 +27,20 @@ public class linkedlist {
         size++;                             // Increasing the size
     }
 
+    // Insert at end
+    public void insertAtEnd(int value){
+        Node newNode = new Node(value);
+        newNode.nextNode = null;
+
+        Node last = headNode;
+        while(last.nextNode != null){
+            last = last.nextNode;
+        }
+        last.nextNode = newNode;
+        size++;
+    }
+
+    // Print list
     public void printList(){
         Node currentNode = headNode;
         while (currentNode.nextNode != null){
@@ -33,14 +49,19 @@ public class linkedlist {
             currentNode = currentNode.nextNode;
         }
         String value = Integer.toString(currentNode.data);
-        System.out.print(value + " -> NULL");
+        System.out.print(value + " -> NULL\n");
     }
     
     public static void main(String[] args) {
         linkedlist myLinkedList = new linkedlist();
         for (int i = 0 ; i < 10 ; i++){
             myLinkedList.insertAtHead(i);
+            myLinkedList.printList();
         }
         myLinkedList.printList();
+
+        myLinkedList.insertAtEnd(100);
+        myLinkedList.printList();
+
     }
 }
