44028ac1a8f442b15139e5ef5ac7a70907a64695
Amitesh Verma <vamitesh2409@gmail.com>
2023-08-26 21:51:11 +0530

    Added solution to some leetcode easy questions

---
+++ Questions/Searching/search_in_rotated_array.java
@@ -0,0 +1,8 @@
+package Questions.Searching;
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+public class search_in_rotated_array {
+    public static void main(String[] args) {
+        int nums[] = {4,5,6,7,0,1,2};
+        int target = 3;
+    }
+}
