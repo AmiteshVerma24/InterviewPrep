43b60712b9e70af6942511da8c5fba56f6884393
Amitesh Verma <vamitesh2409@gmail.com>
2024-10-01 14:25:52 +0530

    Deletion in trie

--- DataStructures/Trie/Implementation/Trie.java
+++ DataStructures/Trie/Implementation/Trie.java
@@ -11,6 +11,15 @@ class Trie{
     public int getIndex(char t){
       return t - 'a';
     }
+
+    //Helper Function to return true if currentNode does not have any children
+    private boolean hasNoChildren(TrieNode currentNode){
+        for (int i = 0; i < currentNode.children.length; i++){
+            if ((currentNode.children[i]) != null)
+                return false;
+        }
+        return true;
+    }
     
     //Function to insert a key,value pair in the Trie
     public void insert(String key)
@@ -63,6 +72,70 @@ class Trie{
         return currentNode.isEndWord;
     }
     
+    //Recursive function to delete given key
+    private boolean deleteHelper(String key, TrieNode currentNode, int length, int level)
+    {
+        boolean deletedSelf = false;
+
+        if (currentNode == null){
+            System.out.println("Key does not exist");
+            return deletedSelf;
+        }
+
+        //Base Case: If we have reached at the node which points to the alphabet at the end of the key.
+        if (level == length){
+            //If there are no nodes ahead of this node in this path
+            //Then we can delete this node
+            if (hasNoChildren(currentNode)){
+                currentNode = null;
+                deletedSelf = true;
+            }
+            //If there are nodes ahead of currentNode in this path
+            //Then we cannot delete currentNode. We simply unmark this as leaf
+            else
+            {
+                currentNode.unMarkAsLeaf();
+                deletedSelf = false;
+            }
+        }
+        else
+        {
+            TrieNode childNode = currentNode.children[getIndex(key.charAt(level))];
+            boolean childDeleted = deleteHelper(key, childNode, length, level + 1);
+            if (childDeleted)
+            {
+                //Making children pointer also null: since child is deleted
+                currentNode.children[getIndex(key.charAt(level))] = null;
+                //If currentNode is leaf node that means currntNode is part of another key
+                //and hence we can not delete this node and it's parent path nodes
+                if (currentNode.isEndWord){
+                    deletedSelf = false;
+                }
+                //If childNode is deleted but if currentNode has more children then currentNode must be part of another key.
+                //So, we cannot delete currenNode
+                else if (!hasNoChildren(currentNode)){
+                    deletedSelf = false;
+                }
+                //Else we can delete currentNode
+                else{
+                    currentNode = null;
+                    deletedSelf = true;
+                }
+            }
+            else
+            {
+                deletedSelf = false;
+            }
+        }
+        return deletedSelf;
+    }
+
     //Function to delete given key from Trie
-    public boolean delete(String key){ return false;}
+    public void delete(String key){
+        if ((root == null) || (key == null)){
+            System.out.println("Null key or Empty trie error");
+            return;
+        }
+        deleteHelper(key, root, key.length(), 0);
+    }
   }
